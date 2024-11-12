441337e82234bb5d2e6e704c26f2dcc1c3626863
saket singh <saketchaudhary2004@gmail.com>
2024-11-12 21:31:58 +0530

    second of today

---
+++ Queuedeque.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.util.LinkedList;
+
+public class Queuedeque {
+
+    static class Queue{
+        Deque<Integer> d = new LinkedList<>();
+
+        public void add(int data){
+            d.addLast(data);
+        }
+
+        public int remove(){
+            return d.removeFirst();
+        }
+
+        public int peek(){
+            return d.getFirst();
+        }
+    }
+    public static void main(String[] args) {
+        Queue q = new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        System.out.println("peek -" + q.peek());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+    }
+}
