80ccb53ab498142b0fddff7556e360df53f50f73
saket singh <saketchaudhary2004@gmail.com>
2024-11-25 22:26:06 +0530

    hashmap

---
+++ Hashmap1.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Hashmap1 {
+    public static void main(String[] args) {
+        // create
+        HashMap<String , Integer> hm = new HashMap<>();
+
+        // Insert O(1)
+        hm.put("India" , 100);
+        hm.put("China" , 150);
+        hm.put("Nepal" , 1);
+        hm.put("US" , 50);
+        hm.put("Indonesia" , 5);
+
+        // // Get O(1)
+        // System.out.println(hm.get("India"));
+
+        // // containsKey O(1)
+        // System.out.println(hm.containsKey("India")); // return true
+        // System.out.println(hm.containsKey("Russia")); // return flase
+        
+        // // remove - O(1)
+        // System.out.println(hm.remove("China"));
+        // System.out.println(hm);
+
+        // // size 
+        // System.out.println(hm.size());
+
+        // // is empty 
+        // System.out.println(hm.isEmpty());
+
+        Set<String> keys = hm.keySet();
+
+        for(String k : keys){
+            System.out.println("key = "+ k + " " +"value = "+ hm.get(k));
+        }
+    }
+}
