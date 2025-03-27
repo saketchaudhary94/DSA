3e84ab14699ca0b72a50bc828b801d789a395aab
saket singh <saketchaudhary2004@gmail.com>
2025-03-27 20:07:41 +0530

    merge sort

--- Sorting.java
+++ Sorting.java
@@ -73,6 +73,44 @@ public class Sorting {
         }
     }
 
+    // merge sort
+    public static void conquer(int arr[], int si , int mid , int ei){
+        int merged[] = new int[ei-si+1];
+        int idx1 = si;
+        int idx2 = mid+1;
+        int x =0;
+
+        while(idx1 <= mid && idx2 <= ei){
+            if(arr[idx1] <= arr[idx2]){
+                merged[x++] = arr[idx1++];
+            }
+            else{
+                merged[x++] = arr[idx2++];
+            }
+        }
+        while(idx1 <= mid){
+            merged[x++] = arr[idx1++];
+        }
+        while(idx2 <= ei){
+            merged[x++] = arr[idx2++];
+        }
+        for(int i=0,j=si;i<merged.length;i++,j++){
+            arr[j] = merged[i];
+
+        }
+
+    }
+
+    public static void divide(int arr[] , int si , int ei){
+        if(si>=ei){
+            return;
+        }
+        int mid = si + (ei - si) /2;
+        divide(arr,si,mid);
+        divide(arr,mid+1,ei);
+        conquer(arr,si,mid,ei);
+    }
+
     // to print an array
     public static void print(int arr[]){
         for(int i =0;i< arr.length;i++){
@@ -82,11 +120,21 @@ public class Sorting {
     }
     public static void main(String[] args) {
         // int arr[] = {5,4,1,2,3};
-        int arr[] = {1,4,1,3,2,4,3,7};
+        // int arr[] = {1,4,1,3,2,4,3,7};
         // bubbleSort(arr);
         // selectionSort(arr);
         // insertionSort(arr);
-        countingSort(arr);
-        print(arr);
+        // countingSort(arr);
+        // print(arr);
+
+        // for merge sort 
+        int arr[] = {6,3,9,5,2,8};
+        int n = arr.length;
+        divide(arr , 0 , n-1);
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
