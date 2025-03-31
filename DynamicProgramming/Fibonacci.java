9d31580453256b80c4b9acca28b9ca30e635edb8
saket singh <saketchaudhary2004@gmail.com>
2025-03-31 14:19:45 +0530

    grid ways recursion

--- DynamicProgramming/Fibonacci.java
+++ DynamicProgramming/Fibonacci.java
@@ -1,5 +1,6 @@
 public class Fibonacci {
 
+    // memoization
     public static int findFibonacci(int n , int f[]){
         if(n == 0 || n == 1){
             return n;
@@ -10,9 +11,21 @@ public class Fibonacci {
         f[n] = findFibonacci(n-1, f) + findFibonacci(n-2, f);
         return f[n];
     }
+
+    // tabulation
+    public static int findFibonacci2(int n){
+        int dp[] = new int[n+1];
+        dp[1] = 1;
+
+        for(int i =2; i <=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
     public static void main(String[] args) {
         int n =6;
         int f[] = new int[n+1];
         System.out.println(findFibonacci(n , f));
+        System.out.println(findFibonacci2(n));
     }
 }
---
+++ Recursion/GridWays.java
@@ -0,0 +1,17 @@
+public class GridWays {
+
+    public static int gridWays(int i , int j , int m , int n){
+        if(i ==  m-1 && j == n-1){
+            return 1;
+        }
+        else if(i ==m || j==n){
+            return 0;
+        }
+
+        return gridWays(i+1, j, m, n) + gridWays(i, j+1, m, n);
+    }
+    public static void main(String[] args) {
+        int m =3 , n =7;
+        System.out.println(gridWays(0, 0, m, n));
+    }
+}
