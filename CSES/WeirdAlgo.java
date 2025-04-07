82481ecc5a0643ecfa158fff76e0d7455f59446f
saket singh <saketchaudhary2004@gmail.com>
2025-04-07 11:20:14 +0530

    sces website codes

---
+++ CSES/WeirdAlgo.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class WeirdAlgo {
+
+    public static void weirdAlgo(long n ){
+        System.out.print(n+" ");
+        while(n >1){
+            if(n%2 == 0){
+                n/=2;
+                System.out.print(n+" ");
+            }
+            else{
+                n = (n*3)+1;
+                System.out.print(n+" ");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        weirdAlgo(n);
+    }
+}
