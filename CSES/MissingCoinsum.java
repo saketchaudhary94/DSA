f7679b0e4c889a5fc269c768c3f6c0b15f0599fb
saket singh <saketchaudhary2004@gmail.com>
2025-04-14 19:15:30 +0530

    sliding window inital commit

--- CSES/MissingCoinsum.java
+++ CSES/MissingCoinsum.java
@@ -1,3 +1,4 @@
+package CSES;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
---
+++ SlidingWindow/MaxPointsFromCards.java
@@ -0,0 +1,25 @@
+public class MaxPointsFromCards {
+
+    public static int maxPointsObtainedFromCards(int arr[] ,int  k){
+        int leftSum = 0 , rightSum = 0 , maxSum = 0;
+
+        for(int i =0;i < k;i++){
+            leftSum += arr[i];
+        }
+        maxSum = leftSum;
+        int rightIdx = arr.length-1;
+
+        for(int i =k-1;i >=0;i--){
+            leftSum -= arr[i];
+            rightSum += arr[rightIdx--];
+
+            maxSum = Math.max(maxSum , leftSum + rightSum);
+        }
+        return maxSum;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,1};
+        int k = 3;
+        System.out.println(maxPointsObtainedFromCards(arr , k));
+    }
+}
